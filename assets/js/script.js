31b20215c2298081e2a4d154aa439d77e8800fef
bryance97 <bryancifuentesescobar@gmail.com>
2021-10-05 18:23:23 -0400

    bryans API

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$('button').on('click', function () {
+    var userInput = $('input').val();
+
+    fetch("https://api.edamam.com/api/recipes/v2?type=public&cuisineType="+userInput+"&app_id=f2f08026&app_key=1c35f8d0041b4d0aee2d8ff722c57ff4"
+    )
+        //conole log request
+        //.then(function (response) {
+        //console.log(response.json();
+
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            console.log(response)
+            for (var i = 0; i < 10; i++) {
+                index = i;
+
+                var displayMealId = $('#display-meal');
+                var imageDiv = $('<div>');
+                var imageTag = $('<img>');
+                var foodTitle = $('<div>');
+                var titleURL = response.hits[index].recipe.label;
+                var foodImageURL = response.hits[index].recipe.image;
+                foodTitle.text(titleURL);
+                imageTag.attr('src', foodImageURL)
+                foodTitle.appendTo(imageDiv);
+                imageTag.appendTo(imageDiv);
+                imageDiv.appendTo(displayMealId)
+            }
+        })
+});
