089fe2cbfa77caceab5459c3b23c4bf4bf6d90a9
briandgomez <briandgomez98@gmail.com>
2021-10-05 21:21:18 -0400

    flexbox and ingredients

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,13 @@ $('button').on('click', function () {
     var userInput = $('input').val();
 
     fetch(
-        "https://api.spoonacular.com/recipes/complexSearch?apiKey=eb00d312a63a45e8bf30e87aca1b8514&cuisine=" + userInput
+        "https://api.spoonacular.com/recipes/complexSearch?apiKey=0e8c7a5527c94d44a604939ba3869efe&cuisine=" + userInput
     )
         .then(function (response) {
             return response.json();
         })
         .then(function (response) {
-            for (var i = 0; i < 10; i++) {
+            for (var i = 0; i < 5; i++) {
                 index = i;
 
                 var displayMealId = $('#display-meal');
@@ -21,8 +21,37 @@ $('button').on('click', function () {
                 imageTag.attr('src', foodImageURL)
                 foodTitle.appendTo(imageDiv);
                 imageTag.appendTo(imageDiv);
-                imageDiv.appendTo(displayMealId)
+                imageDiv.appendTo(displayMealId);
+
+                var foodId = response.results[index].id;
+                getRecipe(foodId);
             }
         })
 })
 
+var getRecipe = function (foodId) {
+
+    fetch(
+        "https://api.spoonacular.com/recipes/" + foodId + "/ingredientWidget.json?apiKey=0e8c7a5527c94d44a604939ba3869efe"
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (response) {
+            var ingredientArray = response.ingredients;
+            //console.log(ingredientArray);
+            for (var i = 0; i < 3; i++) {
+                index = i;
+
+                var ingredientName = response.ingredients[index].name;
+                console.log(ingredientName);
+                var ingredientAmount = response.ingredients[index].amount.us.value;
+                console.log(ingredientAmount);
+
+                var recipeDiv = $('#recipe');
+                var recipePara = $('<p>');
+                recipePara.text(ingredientName + ': ' + ingredientAmount);
+                recipePara.appendTo(recipeDiv);
+            }
+        })
+}
