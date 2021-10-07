1d97054214c6d890ed0d1b36dce7ca66cefbec7f
briandgomez <briandgomez98@gmail.com>
2021-10-07 18:49:14 -0400

    Display 1 image and its recipe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,12 @@
+var displayMealId = $('#display-meal');
+var imageDiv = $('<div>');
+var imageTag = $('<img>');
+var foodTitle = $('<div>');
+var foodIdDiv = $('<div>');
+
+//Click listener for search button
 $('button').on('click', function () {
+    //Input provided by user
     var userInput = $('input').val();
 
     fetch(
@@ -8,29 +16,29 @@ $('button').on('click', function () {
             return response.json();
         })
         .then(function (response) {
-            for (var i = 0; i < 5; i++) {
-                index = i;
+            $('input').val('');
+            foodIdDiv.empty();
 
-                var displayMealId = $('#display-meal');
-                var imageDiv = $('<div>');
-                var imageTag = $('<img>');
-                var foodTitle = $('<div>');
-                var titleURL = response.results[index].title;
+            for (var i = 0; i < 3; i++) {
+                index = i;
+                //Div that contains image and title of specific 'cuisine'
+                var title = response.results[index].title;
                 var foodImageURL = response.results[index].image;
-                foodTitle.text(titleURL);
+                var foodId = response.results[index].id;
+                foodIdDiv.attr('id', foodId);
+                foodTitle.text(title);
                 imageTag.attr('src', foodImageURL)
                 foodTitle.appendTo(imageDiv);
                 imageTag.appendTo(imageDiv);
+                foodIdDiv.appendTo(imageDiv);
                 imageDiv.appendTo(displayMealId);
 
-                var foodId = response.results[index].id;
                 getRecipe(foodId);
             }
         })
 })
 
 var getRecipe = function (foodId) {
-
     fetch(
         "https://api.spoonacular.com/recipes/" + foodId + "/ingredientWidget.json?apiKey=0e8c7a5527c94d44a604939ba3869efe"
     )
@@ -38,20 +46,16 @@ var getRecipe = function (foodId) {
             return response.json();
         })
         .then(function (response) {
-            var ingredientArray = response.ingredients;
-            //console.log(ingredientArray);
+            //Array that contains the ingredients of the specific cuisine
+            //var ingredientArray = response.ingredients;
             for (var i = 0; i < 3; i++) {
                 index = i;
-
                 var ingredientName = response.ingredients[index].name;
-                console.log(ingredientName);
                 var ingredientAmount = response.ingredients[index].amount.us.value;
-                console.log(ingredientAmount);
-
-                var recipeDiv = $('#recipe');
+                var ingredientUnit = response.ingredients[index].amount.us.unit;
                 var recipePara = $('<p>');
-                recipePara.text(ingredientName + ': ' + ingredientAmount);
-                recipePara.appendTo(recipeDiv);
+                recipePara.text(ingredientName + ': ' + ingredientAmount + ' ' + ingredientUnit);
+                recipePara.appendTo(foodIdDiv);
             }
         })
 }
